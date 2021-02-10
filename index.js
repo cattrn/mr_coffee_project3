bc48288090d42e872e56413f5bb31d017033d8fe
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-02-10 11:24:10 +1100

    removed node_modules dir, styling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const crypto = require('crypto')
 const data = require('./public/data.js')
+const crypto = require('crypto')
 
 const app = express()
 const port = 3000
@@ -34,13 +34,12 @@ app.get('/schedules', (req, res) => {
   })
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id(\\d+)/', (req, res) => {
   res.render('user', {
     users: data.users[req.params.id]
   })
 })
 
-//this one doesn't work
 let personalSchedule = []
 app.get('/users/:id/schedules', (req, res) => {
   for (i = 0; i < data.schedules.length; i++) {
@@ -49,42 +48,51 @@ app.get('/users/:id/schedules', (req, res) => {
     }
   }
   res.render('schedule', {
+    users: data.users[req.params.id],
     schedules: personalSchedule
   })
+  personalSchedule = []
+})
 
-  // personalSchedule = []
+app.get('/users/new', (req, res) => {
+  res.render('new-user')
+})
+
+app.get('/schedules/new', (req, res) => {
+  res.render('new-schedule')
 })
 
 // STEP 4 --------------------------------//
 
-app.post('/schedules', (req, res) => {
-  let newSchedule = {
-    'user_id': Number(req.body.user_id),
-    'day': Number(req.body.day),
-    'start_at': req.body.start_at,
-    'end_at': req.body.end_at
-  }
+// app.post('/schedules', (req, res) => {
+//   let newSchedule = {
+//     'user_id': Number(req.body.user_id),
+//     'day': Number(req.body.day),
+//     'start_at': req.body.start_at,
+//     'end_at': req.body.end_at
+//   }
 
-  data.schedules.push(newSchedule)
-  res.send(newSchedule)
-})
+//   data.schedules.push(newSchedule)
+//   res.send(newSchedule)
+// })
 
 
-app.post('/users', (req, res) => {
-  const secret = 'abcdefg'
-  const hash = crypto.createHmac('sha256', secret).update(req.body.password).digest('hex')
 
-  let newUser = {
-    'firstname': req.body.firstname,
-    'lastname': req.body.lastname,
-    'email': req.body.email,
-    'password': hash
-  }
+
+// app.post('/users', (req, res) => {
+//   const secret = 'abcdefg'
+//   const hash = crypto.createHmac('sha256', secret).update(req.body.password).digest('hex')
+
+//   let newUser = {
+//     'firstname': req.body.firstname,
+//     'lastname': req.body.lastname,
+//     'email': req.body.email,
+//     'password': hash
+//   }
   
-  data.users.push(newUser)
-  res.send(newUser)
-})
-
+//   data.users.push(newUser)
+//   res.send(newUser)
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
