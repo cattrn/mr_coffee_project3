f01a7911525233b343c585e391e7f851ea8cd9c8
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-02-12 10:06:20 +1100

    added morgan and connected mysql

--- index.js
+++ index.js
@@ -1,118 +1,138 @@
+// express setup
 const express = require('express')
-const data = require('./public/data.js')
-const crypto = require('crypto')
-
 const app = express()
-const port = 3000
-
-// I don't know how these work
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use(express.static('public'))
+// morgan setup
+const morgan = require('morgan')
+app.use(morgan('dev'))
 
-// pug template engine
-app.set('views', './views')
-app.set('view engine', 'pug')
+// mysql setup
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '12345678',
+  database: 'mrCoffee'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
-// VIEWS
+// // files
+// const data = require('./public/data.js')
+// app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    description: 'Welcome to our schedule website'
-  })
-})
+// // password encryption
+// const crypto = require('crypto')
 
-app.get('/users', (req, res) => {
-  res.render('users', {
-    users: data.users
-  })
-})
+// port
+const port = 3000
 
-app.get('/schedules', (req, res) => {
-  res.render('schedules', {
-    schedules: data.schedules
-  })
-})
+// // pug template engine
+// app.set('views', './views')
+// app.set('view engine', 'pug')
 
-// regex to limit parameter to numbers
-app.get('/users/:id(\\d+)/', (req, res) => {
-  res.render('user', {
-    users: data.users[req.params.id]
-  })
-})
+// // VIEWS
 
-let personalSchedule = []
-app.get('/users/:id/schedules', (req, res) => {
-  for (i = 0; i < data.schedules.length; i++) {
-    if (data.schedules[i]['user_id'] == req.params.id) {
-      personalSchedule.push(data.schedules[i])
-    }
-  }
-  res.render('schedule', {
-    users: data.users[req.params.id],
-    schedules: personalSchedule
-  })
-  personalSchedule = []
-})
+// app.get('/', (req, res) => {
+//   res.render('index', {
+//     description: 'Welcome to our schedule website'
+//   })
+// })
 
-app.get('/users/new', (req, res) => {
-  res.render('new-user')
-})
+// app.get('/users', (req, res) => {
+//   res.render('users', {
+//     users: data.users
+//   })
+// })
 
-app.get('/schedules/new', (req, res) => {
-  res.render('new-schedule', {
-    users: data.users
-  })
-})
+// app.get('/schedules', (req, res) => {
+//   res.render('schedules', {
+//     schedules: data.schedules
+//   })
+// })
 
-// SCHEDULE FORM
+// // regex to limit parameter to numbers
+// app.get('/users/:id(\\d+)/', (req, res) => {
+//   res.render('user', {
+//     users: data.users[req.params.id]
+//   })
+// })
 
-app.post('/schedules', (req, res) => {
-  let user_id = Number
-  for (i = 0; i < data.users.length; i++) {
-    if (req.body.name == data.users[i]['firstname'] + ' ' + data.users[i]['lastname']) {
-      user_id = i
-    }
-  }
+// let personalSchedule = []
+// app.get('/users/:id/schedules', (req, res) => {
+//   for (i = 0; i < data.schedules.length; i++) {
+//     if (data.schedules[i]['user_id'] == req.params.id) {
+//       personalSchedule.push(data.schedules[i])
+//     }
+//   }
+//   res.render('schedule', {
+//     users: data.users[req.params.id],
+//     schedules: personalSchedule
+//   })
+//   personalSchedule = []
+// })
 
-  const weekTranslation = {
-    Monday: 1,
-    Tuesday: 2,
-    Wednesday: 3,
-    Thursday: 4,
-    Friday: 5,
-    Saturday: 6,
-    Sunday: 7
-  }
+// app.get('/users/new', (req, res) => {
+//   res.render('new-user')
+// })
 
-  let newSchedule = {
-    'user_id': user_id,
-    'day': weekTranslation[req.body.day],
-    'start_at': req.body.start_at,
-    'end_at': req.body.end_at
-  }
+// app.get('/schedules/new', (req, res) => {
+//   res.render('new-schedule', {
+//     users: data.users
+//   })
+// })
 
-  data.schedules.push(newSchedule)
-  res.redirect('/schedules')
-})
+// // SCHEDULE FORM
+
+// app.post('/schedules', (req, res) => {
+//   let user_id = Number
+//   for (i = 0; i < data.users.length; i++) {
+//     if (req.body.name == data.users[i]['firstname'] + ' ' + data.users[i]['lastname']) {
+//       user_id = i
+//     }
+//   }
+
+//   const weekTranslation = {
+//     Monday: 1,
+//     Tuesday: 2,
+//     Wednesday: 3,
+//     Thursday: 4,
+//     Friday: 5,
+//     Saturday: 6,
+//     Sunday: 7
+//   }
+
+//   let newSchedule = {
+//     'user_id': user_id,
+//     'day': weekTranslation[req.body.day],
+//     'start_at': req.body.start_at,
+//     'end_at': req.body.end_at
+//   }
+
+//   data.schedules.push(newSchedule)
+//   res.redirect('/schedules')
+// })
 
 
-// USER FORM
+// // USER FORM
 
-app.post('/users', (req, res) => {
-  const secret = 'abcdefg'
-  const hash = crypto.createHmac('sha256', secret).update(req.body.password).digest('hex')
+// app.post('/users', (req, res) => {
+//   const secret = 'abcdefg'
+//   const hash = crypto.createHmac('sha256', secret).update(req.body.password).digest('hex')
 
-  let newUser = {
-    'firstname': req.body.firstname,
-    'lastname': req.body.lastname,
-    'email': req.body.email,
-    'password': hash
-  }
-  data.users.push(newUser)
-  res.redirect('/users')
-})
+//   let newUser = {
+//     'firstname': req.body.firstname,
+//     'lastname': req.body.lastname,
+//     'email': req.body.email,
+//     'password': hash
+//   }
+//   data.users.push(newUser)
+//   res.redirect('/users')
+// })
 
 
 
