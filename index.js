dfd146f13ee6d037cccc65a75c264b322b4082af
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-05-26 10:42:02 +1000

    Add a GET home route

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 3000
 // access to our data.js file
 const data = require('./data')
 
-
+app.get('/', (req, res) => {
+  res.send('Welcome to our schedule website')
+})
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
